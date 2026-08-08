a371f07c26ca9134af0db9c528b313bed5c9b06b
lwango1 <lwangodany@gmail.com>
2026-08-08 15:01:11 +0200

    Pointe r lpp mobile vers le backend en ligne

--- confidence_app/lib/services/api_service.dart
+++ confidence_app/lib/services/api_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiConfig {
-  static const String baseUrl = 'http://10.0.2.2:3000';
+  static const String baseUrl = 'https://confidenceld-backend.onrender.com';
 }
 
 class ApiService {
